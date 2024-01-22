b679c7e7d5a8e6d9cc9c4a3311b6e14b8db0475d
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2024-01-22 10:09:47 +0300

    add github actions to fix issue

--- scripts/page_available_since.py
+++ scripts/page_available_since.py
@@ -1,5 +1,4 @@
-from bs4 import BeautifulSoup
-import os, requests
+import requests
 import re
 import json
 
@@ -7,86 +6,119 @@ versions = [
     {
         "path": "/docs/",
         "name": "Latest - 5.2",
+        "branch": "release-5.2"
     },
     {
         "path": "/docs/5.1/",
         "name": "5.1",
+        "branch": "release-5.1"
 
     },
     {
         "path": "/docs/5.0/",
         "name": "5 LTS",
+        "branch": "release-5"
     },
     {
         "path": "/docs/4.3/",
         "name": "4.3",
+        "branch": "release-4.3"
     },
     {
         "path": "/docs/4.2/",
         "name": "4.2",
+        "branch": "release-4.2"
     },
     {
         "path": "/docs/4.1/",
         "name": "4.1",
+        "branch": "release-4.1"
     },
     {
         "path": "/docs/4.0/",
         "name": "4 LTS",
+        "branch": "release-4"
     },
     {
         "path": "/docs/3.2/",
         "name": "3.2",
+        "branch": "release-3.2"
     },
     {
         "path": "/docs/3.1/",
         "name": "3.1",
+        "branch": "release-3.1"
     },
+    {
+        "path": "/docs/3-lts/",
+        "name": "3 LTS",
+        "branch": "release-3-lts"
+    },
+
     {"path": "/docs/nightly/",
      "name": "Nightly",
+     "branch": "master"
      }
 ]
 
 filePath = "../tyk-docs/data/page_available_since.json"
 
+aliases = set()
 
-def process_and_write_to_file():
-    available = get_site_maps()
+
+def process_and_write_to_file() -> None:
+    available = get_and_process_urls()
+    data_file = {"versions": versions, "pages": available}
     with open(filePath, 'w') as file:
-        json.dump(available, file, indent=4)
+        json.dump(data_file, file, indent=4)
 
 
-def get_site_maps():
+def write_aliases():
+    with open("aliases.txt", 'w') as file:
+        ns = sorted(aliases)
+        for item in ns:
+            file.write(str(item) + '\n')
+
+
+def get_and_process_urls():
     available_since = {}
     for version in versions:
-        url = "https://tyk.io/{version}/sitemap.xml".format(version=version["path"])
-        print("Getting sitemap for {url}".format(url=url))
-        response = requests.get(url, headers={'user-agent': 'insomnia/2023.4.0'})
-        if response.status_code != 200:
-            raise Exception("unable to fetch the sitemap")
-        response.raise_for_status()
-        print("finished fetching for {url}".format(url=url))
-        urls = get_urls(content=response.text)
-        print("parsing urls for version {version}".format(version=version["name"]))
-        for url in urls:
-            if url in available_since:
-                available_since[url].append(version["path"])
+        url = "https://tyk.io{version}pagesurl.json".format(version=version["path"])
+        data = fetch_file(url)
+        if 'pages' in data:
+            pages = data['pages']
+            for page in pages:
+                url = page.get('path')
+                if url:
+                    if not url.startswith('/'):
+                        url = '/' + url
+                    if not url.endswith('/'):
+                        url += '/'
+                parent = page.get("parent")
+                alt_url = url
+                if parent is not None:
+                    alt_url = parent
+                    aliases.add(url)
+                if url not in available_since:
+                    available_since[url] = {}
+                available_since[url][version["path"]] = alt_url
+    for link in aliases:
+        ns = available_since[link]
+        similar = {}
+        diff = {}
+        for key, value in ns.items():
+            if link == value:
+                similar[key] = value
             else:
-                available_since[url] = [version["path"]]
-        print("finished adding to dict urls for version {version}".format(version=version["name"]))
-    return available_since
+                diff[key] = value
+        for dk, dv in diff.items():
+            for sk, sv in similar.items():
+                if sk not in available_since[dv]:
+                    available_since[dv][sk] = sv
+    return dict(sorted(available_since.items()))
 
 
-def get_urls(content: str):
-    parsed = BeautifulSoup(content, "lxml")
-    urls_from_xml = []
-    loc_tags = parsed.find_all('loc')
-    for loc in loc_tags:
-        urls_from_xml.append(replace_base_url(loc.get_text()))
-    urls_from_xml.sort()
-    return sorted(urls_from_xml)
-
-
-def replace_base_url(url: str):
+def replace_base_url(url: str) -> str:
     version_pattern = r'https://tyk\.io/docs/[0-9]+(\.[0-9]+)?'
     replace_nightly = 'https://tyk.io/docs/nightly'
     replace_3lts = 'https://tyk.io/docs/3-lts'
@@ -98,11 +130,20 @@ def replace_base_url(url: str):
     return modified_url
 
 
-# print(get_urls("latest_sitemap.xml"))
-
 def read_file(file_name: str):
-    f = open(file_name, "r")
-    get_urls(content=f.read())
+    with open(file_name, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def fetch_file(url: str):
+    print("Getting sitemap for {url}".format(url=url))
+    response = requests.get(url, headers={'user-agent': 'insomnia/2023.4.0'})
+    if response.status_code != 200:
+        raise Exception("unable to fetch the pagesurl.json")
+    response.raise_for_status()
+    print("finished fetching for {url}".format(url=url))
+    return response.json()
 
 
 def test_modified():
